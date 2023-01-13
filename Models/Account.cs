b5b6889ee13cb824643ffe3db1538cd3804d83eb
victorhugomr <victorhugo_mr@live.com>
2023-01-13 20:14:59 -0300

    Creating classes

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+abstract class Account
+{
+    string login { get; set; }
+    string password { get; set; }
+    string email { get; set; }
+    string CPF { get; set; }
+    string name { get; set; }
+    string photo { get; set; }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,8 @@
+class Order
+{
+    long id { get; }
+    Location location { get; }
+    Employer employer { get; }
+    Housekeeper housekeeper { get; }
+    double price { get; }
+}
